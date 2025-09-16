a35e57a94c862e80ad6cfb3075b78a7d92484839
Jim Verducci <68206824+jim-verducci@users.noreply.github.com>
2025-09-15 23:19:15 -0700

    Use SDK auto configuration; use decorator/mixin on protected views (#3)
    * Use SDK auto configuration; use decorator/mixin on protected views
    
    * Reset dev mode

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     path("hello-world/", views.HelloWorld.as_view(), name="hello_world"),
     path("profile/", views.Profile.as_view(), name="profile"),
     # API Endpoints
-    path("api/cookie-hello/", views.CookieHelloWorldApi.as_view(), name="api_cookie_hello"),
-    path("api/token-hello/", views.TokenHelloWorldApi.as_view(), name="api_token_hello"),
+    path("api/cookie-hello/", views.cookie_hello_world_api, name="api_cookie_hello"),
+    path("api/token-hello/", views.token_hello_world_api, name="api_token_hello"),
     # __WRISTBAND__: Auth Endpoints
-    path("auth/login/", views.Login.as_view(), name="login"),
-    path("auth/callback/", views.Callback.as_view(), name="callback"),
-    path("auth/logout/", views.Logout.as_view(), name="logout"),
+    path("auth/login/", views.login_view, name='login'),
+    path("auth/callback/", views.callback_view, name='callback'),
+    path("auth/logout/", views.logout_view, name='logout'),
 ]
--- demo_app/views/__init__.py
+++ demo_app/views/__init__.py
@@ -1,6 +1,15 @@
-from .api_views import CookieHelloWorldApi, TokenHelloWorldApi
-from .auth_views import Callback, Login, Logout
+from .api_views import cookie_hello_world_api, token_hello_world_api
+from .auth_views import callback_view, login_view, logout_view
 from .page_views import HelloWorld, Home, Profile
 
 # Explicit exports
-__all__ = ["Callback", "HelloWorld", "CookieHelloWorldApi", "TokenHelloWorldApi", "Home", "Login", "Logout", "Profile"]
+__all__ = [
+  "callback_view",
+  "cookie_hello_world_api",
+  "HelloWorld",
+  "Home",
+  "login_view",
+  "logout_view",
+  "Profile",
+  "token_hello_world_api",
+]
